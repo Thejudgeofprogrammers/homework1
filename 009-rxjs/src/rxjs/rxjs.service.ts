88c2786461b41043825fda35820159e49da989b3
Thejudgeofprogrammers <temka2004@hotmail.com>
2024-07-30 21:24:02 +0300

    homework_rxjs

--- 009-rxjs/src/rxjs/rxjs.service.ts
+++ 009-rxjs/src/rxjs/rxjs.service.ts
@@ -13,21 +13,36 @@ import axios from "axios";
 @Injectable()
 export class RxjsService {
   private readonly githubURL = "https://api.github.com/search/repositories?q=";
+  private readonly gitlabURL = "https://gitlab.com/api/v4/projects?search=";
+
 
   private getGithub(text: string, count: number): Observable<any> {
-    return from(axios.get(`${this.githubURL}${text}`))
-      .pipe(
+    return from(axios.get(`${this.githubURL}${text}`)).pipe(
         map((res: any) => res.data.items),
         mergeAll(),
-      )
-      .pipe(take(count));
-  }
+        take(count)
+      );
+  };
+
+  private getGitlab(text: string, count: number): Observable<any> {
+    return from(axios.get(`${this.gitlabURL}${text}`)).pipe(
+      map((res: any) => res.data),
+      mergeAll(),
+      take(count)
+    );
+  };
 
   async searchRepositories(text: string, hub: string): Promise<any> {
-    // Здесь можно добавить логику проверки на какой hub делать запрос
-    console.log("hub = ", hub);
-    const data$ = this.getGithub(text, 10).pipe(toArray());
-    data$.subscribe(() => {});
+    let data$: Observable<any>;
+
+    if (hub === 'github') {
+      data$ = this.getGithub(text, 10).pipe(toArray());
+    } else if (hub === 'gitlab') {
+      data$ = this.getGitlab(text, 10).pipe(toArray());
+    } else {
+      throw new Error('Unsupported hub');
+    }
+
     return await firstValueFrom(data$);
-  }
-}
+  };
+};
